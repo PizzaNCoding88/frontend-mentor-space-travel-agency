44678752c9f6cc6ce56ee5886bf31f54a4c38d56
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-07-09 17:33:35 +0100

    setup halfway

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        darkNavy: "#0B0D17",
+        lightBlue: "#D0D6F9",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        bellefair: ["var(--font-bellefair)"],
+        barlowCondensed: ["var(--font-barlow-condensed)"],
+        barlow: ["var(--font-barlow)"],
       },
     },
   },
