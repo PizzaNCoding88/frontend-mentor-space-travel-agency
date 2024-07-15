62c4667aa4777b561d2ab5792b3fb7c806f5d8f8
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-07-15 17:16:02 +0100

    mobile - sidebar path indicator

--- app/components/Home/sidebar/Sidebar.jsx
+++ app/components/Home/sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import SideBar from "./Sidebar.module.css";
 import { usePathname } from "next/navigation";
+import "./sidebar.css";
 
 const Sidebar = (props) => {
   const { status } = props;
   const pathname = usePathname();
+  let path = 0;
+  if (pathname == "/") {
+    path = 1;
+  } else if (pathname == "/destination") {
+    path = 2;
+  } else if (pathname == "/crew") {
+    path = 3;
+  } else if (pathname == "/technology") {
+    path = 4;
+  }
   return (
     <>
       <div className={SideBar.sidebar}>
@@ -15,25 +26,25 @@ const Sidebar = (props) => {
           </button>
         </div>
         <ul>
-          <li>
-            <a className={SideBar.a} href="/">
+          <li className={`${path == 1 ? "pageIndicator" : ""}`}>
+            <a className={SideBar.links} href="/">
               <p className={SideBar.count}>00</p>
               <p className={SideBar.name}>Home</p>
             </a>
           </li>
-          <li>
+          <li className={`${path == 2 ? "pageIndicator" : ""}`}>
             <a href="/destination">
               <p className={SideBar.count}>01</p>
               <p className={SideBar.name}>Destination</p>
             </a>
           </li>
-          <li>
+          <li className={`${path == 3 ? "pageIndicator" : ""}`}>
             <a href="">
               <p className={SideBar.count}>02</p>
               <p className={SideBar.name}>Crew</p>
             </a>
           </li>
-          <li>
+          <li className={`${path == 4 ? "pageIndicator" : ""}`}>
             <a href="">
               <p className={SideBar.count}>03</p>
               <p className={SideBar.name}>Technlogy</p>
