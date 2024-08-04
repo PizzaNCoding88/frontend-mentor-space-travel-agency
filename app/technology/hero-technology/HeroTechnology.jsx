945f9afecede6c71d94484e5ff16ba76f2681faf
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-08-04 21:34:28 +0100

    mobile - technology page 0.2

---
+++ app/technology/hero-technology/HeroTechnology.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Hero from "./HeroTechnology.module.css";
+
+const HeroTechnology = () => {
+  return (
+    <div>
+      <div className={Hero.header}>
+        <p>03</p>
+        <h1>space launch 101</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroTechnology;
--- app/technology/page.js
+++ app/technology/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Technology from "./Technology.module.css";
 import MobileNavBar from "../components/Home/mobile-navbar/MobileNavBar";
+import HeroTechnology from "./hero-technology/HeroTechnology";
 
 const page = () => {
   return (
     <>
       <main className={Technology.main}>
         <MobileNavBar />
+        <HeroTechnology />
       </main>
     </>
   );
